5da67ec1a72ad76165a995dc4d3a48107cc93dc1
zhangjingguang <2586300277@qq.com>
2025-04-28 12:55:54 +0800

    test

--- config/github-examples.js
+++ config/github-examples.js
@@ -10,12 +10,25 @@ export default [
                 id: 'test',
                 name: '点击购买',
                 tag: '1元',
+                tip: '此案例是一个简单的开发者出售的示例',
                 author: 'z2586300277',
                 githubUrl: 'https://m.tb.cn/h.6lQpi4g?tk=A7vjVePK4zN ',
-                openUrl: 'aa',
+                openUrl: 'https://z2586300277.github.io/three-editor/dist/#/example',
                 image: HOST + 'images/wx_star.png',
+                referUrl: 'https://pan.quark.cn/s/201da5c82fec',
+                links: [
+                    {
+                        name: '📖说明文档',
+                        url: 'https://z2586300277.github.io/blog/'
+                    },
+                    {
+                        name: '📺讲解视频',
+                        url: 'https://www.bilibili.com/video/BV1F6LLzKEqw/'
+                    },
+
+                ],
                 // improver: 'Threejs',
-                // referUrl: 'https://m.tb.cn/h.6lQpi4g?tk=A7vjVePK4zN',
+                
             }
         ]
 
--- config/host.js
+++ config/host.js
@@ -37,7 +37,8 @@ if (localStorage.getItem('KNOWTIPMarket') != formattedDate) {
     <p> 此页可帮助开发者放置您出售的作品或者案例。🍃</p>
     <div>售卖链接（淘宝/咸鱼）或收款方式（微信/支付宝）请使用您个人的，完全免费🍀。</div>
     <p> 希望能帮助到所有三维可视化开发者在业余盈利 🍁</p>
-    <div>提交方式 <a href="https://github.com/OpenThree/market" target="_blank">链接</a>，有问题请咨询 : <a href="https://z2586300277.github.io/" target="_blank">优雅永不过时</a> ✋</div>
+    <div>作品添加方式： <a href="https://github.com/OpenThree/market" target="_blank">点击跳转</a> ✋</div>
+    <p>问题咨询 : <a href="https://z2586300277.github.io/" target="_blank">优雅永不过时</a> 😊</p>
 `;
     Object.assign(closeBtn.style, {
         background: 'none',
