c943c922c1e6f1619a5dcc91285af1b803f3667b
nicomacbookpro <805879871@qq.com>
2025-04-30 10:22:31 +0800

    修改

--- config/github-examples.js
+++ config/github-examples.js
@@ -28,8 +28,32 @@ export default [
 
                 ],
                 // improver: 'Threejs',
-                
+
+            },
+            {
+                id: 'the ship',
+                name: '免费',
+                tag: '免费',
+                tip: '船似浮萍海上漂',
+                author: 'nico',
+                githubUrl: 'https://m.tb.cn/h.6lQpi4g?tk=A7vjVePK4zN ',
+                // openUrl: 'https://nicowebgl.cn/bolt/ocean/',
+                openUrl: 'https://z2586300277.github.io/three-cesium-examples/#/codeMirror?navigation=ThreeJS&classify=application&id=phy,ocean',
+                image: HOST + 'images/nico_wx.jpg',
+                // referUrl: 'https://pan.quark.cn/s/201da5c82fec',
+            },
+            {
+                id: 'the ship 2',
+                name: '赞赏时留言购买',
+                tag: '100元',
+                tip: '船的航行仿真',
+                author: 'nico',
+                githubUrl: 'https://github.com/Nicolas-zn',
+                openUrl: 'https://nicowebgl.cn/ocean_three/ocean/',
+                image: HOST + 'images/nico_wx.jpg',
+                // referUrl: 'https://pan.quark.cn/s/201da5c82fec',
             }
+
         ]
 
     }
